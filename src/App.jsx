f04e2ef848420acbce2dcc6c1b5d7ecc4a127f23
swgicho <soso5@kumoh.ac.kr>
2025-10-02 16:12:33 +0900

    ✨주식 정보 얻기

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import TickerCard from './TickerCard'
 
-const stocks = [
-  { ticker: 'NVDA', name: '엔비디아', currentPrice: 178.43, previousClose: 181.40 },
-  { ticker: 'TSLA', name: '테슬라', currentPrice: 425.85, previousClose: 439.95 },
-  { ticker: 'GOOGL', name: '알파벳', currentPrice: 251.66, previousClose: 253.22 },
-//  { ticker: '005930.KS', name: '삼성전자', currentPrice: 85400, previousClose: 84700 },
+const stocks = [  
+  { ticker: 'NVDA' },
+  { ticker: 'TSLA' },
+  { ticker: 'GOOGL' },
+  { ticker: 'QQQM1' }, // 임시로 오류 발생시키기
+  { ticker: 'SPLG' }, 
+  // { ticker: '005930.KS' },
 ]
 
 function App() {
@@ -18,9 +20,6 @@ function App() {
             <TickerCard 
               key={stock.ticker} 
               ticker={stock.ticker} 
-              name={stock.name} 
-              currentPrice={stock.currentPrice} 
-              previousClose={stock.previousClose}
             />
           ))
         }
